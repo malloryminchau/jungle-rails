35f94ad4c09aff244d5685c3dbf07d588e248fdf
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-10-16 23:23:35 +0000

    admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: 'Jungle', password: 'book'
   def show
+    @products = Product.order(:category_id).all
   end
 end
