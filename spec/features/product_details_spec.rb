071f6d77ce58d2fa926f3a07aacdf5c88a502ce6
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-10-22 21:33:42 +0000

    details page testing

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+require 'capybara/rails'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "See product details" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    
+
+    click_on 'Details »', :match => :first
+    
+    expect(page).to have_css "section.products-show"
+    sleep 5
+    save_screenshot
+  end
+
+end
